d6fe0fb1e041d4d895d19bf51339bb2aa85f9881
unknown <Sukun@AI-CNS-SUKUNKO.LGE.NET>
2023-09-07 11:05:57 -0500

    Installed svelte simple datatable package

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,9 @@
 <script>
-	export let name;
+	import Component from "..Component";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Component />
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<svelte:head>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</svelte:head>
